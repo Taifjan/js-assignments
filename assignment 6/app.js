487c632220adf9cdc591f6c62646b4b7e069dfd7
TAIF JAN <154822433+Taifjan@users.noreply.github.com>
2024-03-15 19:49:23 -0700

    code update

---
+++ assignment 6/app.js
@@ -0,0 +1,59 @@
+// answer 1
+var taif = [];
+taif .push("taif","waseem","muhammad")
+console.log(taif);
+  
+
+//   answer 3
+
+var strings = ["apple", "banana", "mango"];
+console.log(strings);
+// answer 4
+
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// answer 5
+
+var boolean = [true, false,];
+console.log(boolean);
+
+
+// answer 6
+
+var mixed = ["string", 123, true, null, undefined];
+console.log(mixed);
+
+
+// answer 7
+
+
+var  qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"];
+
+document.write("<h2>Educational Qualifications in Pakistan:</h2>",qualifications);
+
+// answer 11
+
+var cities = ["karachi", "Lahore", "islamabad", "multan", "sukker"];
+
+var selectedCities = [];
+
+selectedCities = cities.slice(0, 3);
+
+console.log("Selected cities:", selectedCities);
+
+// answer 12
+
+ var arr = ["This", "is", "my", "cat"];
+
+console.log(arr);
+
+var device =[ "keyboard" ,"mouse", "printer", "monitor" ]
+
+console.log("Device:",device)
+
+// answer 13
+
+var store = [ "apple", "Motorola,", "samsung", "Nokia," ,"Sony" ,"Haier" ]
+
+console.log(store);
